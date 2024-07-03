f9be54162d584b93b0dce7ff04b8f0199aa37dd3
Vikram Naik <26vikramnaik@gmail.com>
2024-07-03 22:32:02 +0530

    fix(hooks): before and after hooks

--- features/step-definitions/step.js
+++ features/step-definitions/step.js
@@ -1,7 +1,15 @@
-import { When, Then } from '@cucumber/cucumber';
+import { When, Then, Before, After } from '@cucumber/cucumber';
 import { expect } from "chai"
 
 let sum = 0
+
+Before(function() {
+  console.log("Before")
+})
+
+After(function() {
+  console.log("After")
+})
 
 When('I add {int} and {int}', function(int, int2) {
   sum = int + int2
